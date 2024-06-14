4d523b28f95df27676cab0427855ed66f56ce6d9
Swati-Kanathe <swkanathe@bestpeers.com>
2024-06-14 18:23:26 +0530

    changes in validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,8 +1,8 @@
 class Country < ApplicationRecord
-  validates :name,uniqueness: true, acceptance: { message: 'must be abided' } 
-  
-  validates :capital_city , uniqueness: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  validates :population, length: { in: 6..12 }
-  validates :name, confirmation: true
+  validates :name, presence: { strict: true }
   validates :name_confirmation, presence: true
+  validates :capital_city , uniqueness: true
+  validates :population, length: { in: 6..12 }
+  after_create -> { puts "Congratulations!" }
 end
+
